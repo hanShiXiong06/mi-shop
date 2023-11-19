f04c282d5f101a0d1ed96f9a4213aec5499c1d81
hanshixiong <hanshixiong@icloud.com>
2023-11-19 15:52:55 +0800

    修改了数据库的文件 , 添加了几个表及测试数据

--- api/cars.sql
+++ api/cars.sql
@@ -1,19 +1,15 @@
-/*
-Navicat MySQL Data Transfer
 
-Source Server         : yjyan
-Source Server Version : 50725
-Source Host           : bdm721864350.my3w.com:3306
-Source Database       : bdm721864350_db
-
-Target Server Type    : MYSQL
-Target Server Version : 50725
-File Encoding         : 65001
-
-Date: 2022-07-20 22:48:21
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
+CREATE TABLE Categories (
+                            CategoryID INT PRIMARY KEY,
+                            CategoryName VARCHAR(100)
+);
+INSERT INTO Categories (CategoryID, CategoryName) VALUES
+                                                      (1, '电子产品'),
+                                                      (2, '书籍'),
+                                                      (3, '服装'),
+                                                      (4, '食品');
 
 -- ----------------------------
 -- Table structure for cars
@@ -40,3 +36,26 @@ INSERT INTO `cars` VALUES ('3', 'Xiaomi 12S Pro ', 'https://www.yjyan.cn/mi/car3
 INSERT INTO `cars` VALUES ('4', '\r\n小米小爱音箱Play ', 'https://www.yjyan.cn/mi/car1.webp', '增强版 黑色', '149.00', '1', '0', '0');
 INSERT INTO `cars` VALUES ('5', '\r\n\r\nXiaomi 12X ', 'https://www.yjyan.cn/mi/car2.webp', '8GB+256GB 黑色', '3299.00', '1', '0', '0');
 INSERT INTO `cars` VALUES ('6', 'Xiaomi 12S Pro ', 'https://www.yjyan.cn/mi/car3.webp', '8GB+128GB 白色', '4699.00', '1', '0', '0');
+
+CREATE TABLE Users (
+                       UserID INT PRIMARY KEY,
+                       UserName VARCHAR(50),
+                       UserEmail VARCHAR(100)
+);
+INSERT INTO Users (UserID, UserName, UserEmail) VALUES
+                                                    (1, '张三', 'zhangsan@example.com'),
+                                                    (2, '李四', 'lisi@example.com'),
+                                                    (3, '王五', 'wangwu@example.com'),
+                                                    (4, '赵六', 'zhaoliu@example.com');
+
+CREATE TABLE Addresses (
+                           AddressID INT PRIMARY KEY,
+                           UserID INT,
+                           Address VARCHAR(255),
+                           FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+INSERT INTO Addresses (AddressID, UserID, Address) VALUES
+                                                       (1, 1, '北京市朝阳区'),
+                                                       (2, 2, '上海市浦东新区'),
+                                                       (3, 3, '广州市天河区'),
+                                                       (4, 4, '深圳市南山区');
